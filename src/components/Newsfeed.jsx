18ddcdef3784d774179fd31d8383b3cf656b4268
Jmans06 <jmans06@unc.edu>
2024-11-02 16:20:28 -0400

    Newsfeed added

---
+++ src/components/Newsfeed.jsx
@@ -0,0 +1,12 @@
+
+const Newsfeed =()=>{
+    return (
+        <><p>Stay up to date with the latest ABC News hurricane reports</p><iframe
+            id="NewsFeed"
+            title="ABC News Hurricane Reports"
+            width="900"
+            height="2000"
+            src="https://abcnews.go.com/alerts/hurricanes"></iframe></>
+    )
+}
+export default Newsfeed
