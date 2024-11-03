64ee2283939094dd4aa11a4c5dfcc8dac835e1ce
Teddy <tcbarzyk@gmail.com>
2024-11-02 22:23:28 -0400

    started form

--- src/components/EmergencyPlan.jsx
+++ src/components/EmergencyPlan.jsx
@@ -1,30 +1,59 @@
-import { VStack } from "@chakra-ui/react"
-import { Heading } from "@chakra-ui/react"
-import { Button, Input, Stack, Box } from "@chakra-ui/react"
 import { Field } from "./ui/field"
 import { useForm } from "react-hook-form"
+import { Heading, VStack, Box, Link, Strong, Button, Input, Stack } from "@chakra-ui/react"
+import {
+  HoverCardArrow,
+  HoverCardContent,
+  HoverCardRoot,
+  HoverCardTrigger,
+} from "./ui/hover-card"
+import { useState } from "react"
+import { LuInfo } from "react-icons/lu"
 
 const EmergencyPlan = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
 
     return (
-        <Box display="flex" justifyContent="center" alignItems="center" minH="100vh">
-            <VStack width="400px" spacing="8px">
-                <Heading paddingX="50px" color="black" textStyle="5xl">Create Emergency Plan</Heading>
-                <form onSubmit={onSubmit}>
-                    <Stack gap="4" align="flex-start" maxW="sm">
-                        <Field label="Location">
-                            <Input
-                                {...register("location")}
-                            />
-                        </Field>
-                        <Button type="submit">Save Plan</Button>
-                    </Stack>
-                </form>
-            </VStack>
+      <Box display="flex" justifyContent="center" minHeight="100vh">
+        <Box textAlign="left" display="flex" justifyContent="center" padding="20px" width="800px" bg="gray.100">
+          <Box textAlign="left">
+            <Heading color="black" textStyle="5xl">Create Emergency Plan</Heading>
+            <form onSubmit={onSubmit}>
+              <Stack gap="4" align="flex-start" maxW="sm">
+                <Field label="Meeting Location">
+                  <Info text="You should have a meeting location"/>
+                    <Input
+                      {...register("location")}
+                        />
+                </Field>
+                <Button type="submit">Save Plan</Button>
+              </Stack>
+            </form>
+          </Box>
         </Box>
+      </Box>
     )
 }
+
+const Info = ( {text} ) => {
+  const [open, setOpen] = useState(false)
+  return (
+    <HoverCardRoot size="sm" open={open} onOpenChange={(e) => setOpen(e.open)}>
+      <HoverCardTrigger asChild>
+        <Button size="xs" variant="ghost">
+          <LuInfo />
+        </Button>
+      </HoverCardTrigger>
+      <HoverCardContent maxWidth="240px">
+        <HoverCardArrow />
+        <Box>
+          {text}
+        </Box>
+      </HoverCardContent>
+    </HoverCardRoot>
+  )
+}
+
 
 export default EmergencyPlan
